03d57b2063695255cfe21b26cb994cecb70f1f5a
VanjaJuric <117086591+VanjaJuric@users.noreply.github.com>
2022-12-13 15:37:08 +0100

    loggin

--- src/Form/CardContentType.php
+++ src/Form/CardContentType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class CardContentType extends AbstractType
 {
@@ -19,7 +21,22 @@ class CardContentType extends AbstractType
         $builder
             ->add('card_title')
             ->add('card_text')
-            ->add('card_pic')
+            ->add('card_pic', FileType::class,[
+                'label' => 'card_pic (PDF file)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' =>'1024k',
+                        'mimeTypes' =>[
+                            'image/png',
+                            'image/jpg',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid picture',
+                    ])
+                ]
+            ])
             ->add('fk_university', ChoiceType::class, ['choices' => ["1" => "TU Wien"], 'attr' => ['class' => 'form-control my-1 mb-2 w-50']])
             ->add('fk_subject', ChoiceType::class, ['choices' => ["Math" => "1", "Biology" => "2"], 'attr' => ['class' => 'form-control my-1 mb-2 w-50']])
         ;
