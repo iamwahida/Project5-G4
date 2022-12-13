6572f670c7ac299b06c576e15b50eb2df322277e
VanjaJuric <117086591+VanjaJuric@users.noreply.github.com>
2022-12-13 15:39:52 +0100

    logout

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -47,7 +47,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('/card/content'));
+        return new RedirectResponse($this->urlGenerator->generate('app_card_content_index'));
         
     }
 
