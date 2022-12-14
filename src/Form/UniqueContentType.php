e1ac15bd86c09a1c97f4d32073f2fef9fdb3f4b2
VanjaJuric <117086591+VanjaJuric@users.noreply.github.com>
2022-12-14 14:11:04 +0100

    pic unique content

--- src/Form/UniqueContentType.php
+++ src/Form/UniqueContentType.php
@@ -6,6 +6,9 @@ use App\Entity\UniqueContent;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class UniqueContentType extends AbstractType
 {
@@ -15,8 +18,38 @@ class UniqueContentType extends AbstractType
             ->add('first_name')
             ->add('last_name')
             ->add('description')
-            ->add('tut_pic')
-            ->add('bg_pic')
+            ->add('tut_pic',FileType::class,[
+                'label' => 'TuT_Pic (PDF file)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' =>'2048k',
+                        'mimeTypes' =>[
+                            'image/png',
+                            'image/jpg',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid picture',
+                    ])
+                ]
+            ])
+            ->add('bg_pic', FileType::class,[
+                'label' => 'BG_Pic (PDF file)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' =>'2048k',
+                        'mimeTypes' =>[
+                            'image/png',
+                            'image/jpg',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid picture',
+                    ])
+                ]
+            ] )
         ;
     }
 
