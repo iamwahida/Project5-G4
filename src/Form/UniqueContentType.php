bca1033bcd55cfe6833527df8afc13047b6cb51f
VanjaJuric <117086591+VanjaJuric@users.noreply.github.com>
2022-12-14 14:30:35 +0100

    fix

--- src/Form/UniqueContentType.php
+++ src/Form/UniqueContentType.php
@@ -16,8 +16,7 @@ class UniqueContentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-<<<<<<< HEAD
-<<<<<<< HEAD
+
             ->add('first_name')
             ->add('last_name')
             ->add('description')
@@ -53,32 +52,7 @@ class UniqueContentType extends AbstractType
                     ])
                 ]
             ] )
-=======
-            ->add('first_name' , TextType::class, [
-                'attr' => ['class' => 'form-control border border-dark', 'style' => 'margin-bottom:15px'],
-                ],
-               )
-            ->add('last_name', TextType::class, [
-                'attr' => ['class' => 'form-control border border-dark', 'style' => 'margin-bottom:15px'],
-                ],)
-            ->add('description', TextType::class, [
-                'attr' => ['class' => 'form-control border border-dark', 'style' => 'margin-bottom:15px'],
-                ],)
-            ->add('tut_pic', TextType::class, [
-                'attr' => ['class' => 'form-control border border-dark', 'style' => 'margin-bottom:15px'],
-                ],)
-            ->add('bg_pic', TextType::class, [
-                'attr' => ['class' => 'form-control border border-dark', 'style' => 'margin-bottom:15px'],
-                ],)
->>>>>>> b4ad69b84ce851aaae3f59c38b36491851a675da
-=======
-            ->add('first_name')
-            ->add('last_name')
-            ->add('description')
-            ->add('tut_pic')
-            ->add('bg_pic')
->>>>>>> 0d1f2d9c50f1df5b8ebc49f8c64a4d2d9f4aac72
-        ;
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
