91c8ea437c0d6ce5ac092fcd021cd4109b4cf625
iamwahida <nemati.w@yahoo.com>
2022-12-14 14:12:57 +0100

    deleted

--- src/Form/UniqueContentType.php
+++ src/Form/UniqueContentType.php
@@ -13,22 +13,11 @@ class UniqueContentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('first_name' , TextType::class, [
-                'attr' => ['class' => 'form-control border border-dark', 'style' => 'margin-bottom:15px'],
-                ],
-               )
-            ->add('last_name', TextType::class, [
-                'attr' => ['class' => 'form-control border border-dark', 'style' => 'margin-bottom:15px'],
-                ],)
-            ->add('description', TextType::class, [
-                'attr' => ['class' => 'form-control border border-dark', 'style' => 'margin-bottom:15px'],
-                ],)
-            ->add('tut_pic', TextType::class, [
-                'attr' => ['class' => 'form-control border border-dark', 'style' => 'margin-bottom:15px'],
-                ],)
-            ->add('bg_pic', TextType::class, [
-                'attr' => ['class' => 'form-control border border-dark', 'style' => 'margin-bottom:15px'],
-                ],)
+            ->add('first_name')
+            ->add('last_name')
+            ->add('description')
+            ->add('tut_pic')
+            ->add('bg_pic')
         ;
     }
 
